c03f9daa1433d7dcfa8ea746d7ad0aba2d4647be
Omar Ortiz Meraz <omar@ortizmeraz.com>
2024-02-16 12:19:43 -0500

    updates

---
+++ setup.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nala -y
+sudo nala install neofetch -y
+sudo nala install python3 -y
+sudo nala install curl -y
+sudo nala install ranger -y
+sudo nala install btop -y
+sudo nala install exa -y
+sudo nala install tmux -y
+sudo nala install fish -y
+sudo nala install bat -y
+
+curl -sS https://starship.rs/install.sh | sh
+
+
+mkdir -p ~/.config 
+
+
+sudo nala install python3-pip
+
+rm /home/omar/.bashrc
+curl -o /home/omar/.config/fish/config.fish https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/.bashrc
+
+rm /home/omar/.config/fish/config.fish
+curl -o /home/omar/.config/fish/config.fish https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/config.fish
+
+rm /home/omar/.config/starship.toml
+curl -o /home/omar/.config/starship.toml https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/starship.toml
+
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+mkdir -p ~/.config/tmux
