0cd25207d18b0a881871de4ef9a1cecc5041b391
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-02-17 12:06:05 -0500

    Create Setup.ps1

---
+++ Setup.ps1
@@ -0,0 +1,36 @@
+# curl https://ninite.com/7zip-chrome-discord-firefox-foxit-handbrake-itunes-operaChromium-spotify-steam-teracopy-vlc-vscode-windirstat-zoom/ninite.exe --output G:/Downloads/Ninite.exe
+
+# curl https://github.com/balena-io/etcher/releases/download/v1.7.9/balenaEtcher-Setup-1.7.9.exe?d_id=5b68dee9-631c-4ef2-81bd-7344f640b52aR --output balena.exe
+
+
+# Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install flow-launcher
+choco install vscodium
+choco install marktext
+choco install zotero
+choco install github-desktop
+choco install vscode
+choco install ferdium
+choco install vivaldi
+choco install brave
+choco install chromium
+choco install qgis-ltr
+choco install discord
+choco install microsoft-teams
+choco install microsoft-windows-terminal
+choco install powertoys
+choco install touchportal
+choco install python3
+choco install calibre
+
+
+#  Epic
+wget https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
+
+# Office
+wget https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365HomePremRetail&platform=X64&language=en-US&TaxRegion=pr&correlationId=23215db0-0f67-41ed-a36a-4037fea53798&token=03d85092-1323-4cca-a85e-605a0201e689&version=O16GA&source=AMC&StoreId=CFQ7TTC0K5DM
+
+# Mouse with borders
+# wget https://www.microsoft.com/en-ca/download/confirmation.aspx?id=35460
