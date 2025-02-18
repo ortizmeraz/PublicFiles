d6bc077565640c4ed25330b1d63604a9c1c187cf
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-02-18 16:40:08 -0500

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -7,7 +7,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install flow-launcher -a
-choco install vscodium -a
+choco install vlc -a
+choco install vscodium -aº
 choco install marktext -a
 choco install zotero -a
 choco install github-desktop -a
