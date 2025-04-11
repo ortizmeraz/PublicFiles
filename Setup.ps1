28f5356cf03967a7ee0d17e515ee864ca7e4e684
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-04-11 11:33:34 -0400

    updates

--- Setup.ps1
+++ Setup.ps1
@@ -1,44 +1,52 @@
-# curl https://ninite.com/7zip-chrome-discord-firefox-foxit-handbrake-itunes-operaChromium-spotify-steam-teracopy-vlc-vscode-windirstat-zoom/ninite.exe --output G:/Downloads/Ninite.exe
+# Ensure the script is running as Administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator."
+    break
+}
 
-# curl https://github.com/balena-io/etcher/releases/download/v1.7.9/balenaEtcher-Setup-1.7.9.exe?d_id=5b68dee9-631c-4ef2-81bd-7344f640b52aR --output balena.exe
+# Set execution policy to allow script execution
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
+# Install Chocolatey
+Write-Output "Installing Chocolatey..."
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Refresh environment variables to recognize choco command
+$env:Path += ";$env:ALLUSERSPROFILE\chocolatey\bin"
 
-choco install flow-launcher -a
-choco install vlc -a
-choco install vscodium -aº
-choco install marktext -a
-choco install zotero -a
-choco install github-desktop -a
-choco install vscode -a
-choco install ferdium -a
-choco install vivaldi -a
-choco install brave -a
-choco install chromium -a
-choco install qgis-ltr -a
-choco install discord -a
-choco install microsoft-teams -a
-choco install microsoft-windows-terminal -a
-choco install powertoys -a
-choco install touchportal -a
-choco install calibre -a
-choco install calibre -a
-choco install calibre -a
-choco install calibre -a
-choco install git  -a
-choco install obsidian  -a
-choco install steam  -a
-choco install foxitreader  -a
-choco install handbrake -a
-choco install firefox -a
+# List of packages to install
+$packages = @(
+    "brave",
+    "calibre",
+    "chromium",
+    "discord",
+    "ferdium",
+    "firefox",
+    "flow-launcher",
+    "foxitreader",
+    "git",
+    "github-desktop",
+    "handbrake",
+    "marktext",
+    "microsoft-teams",
+    "obsidian",
+    "pdf24",
+    "powertoys",
+    "qgis-ltr",
+    "revo-uninstaller",
+    "starship",
+    "steam",
+    "vivaldi",
+    "vlc",
+    "vscode",
+    "vscodium",
+    "waterfox",
+    "zotero"
+)
 
-#  Epic
-wget https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
-
-# Office
-wget https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365HomePremRetail&platform=X64&language=en-US&TaxRegion=pr&correlationId=23215db0-0f67-41ed-a36a-4037fea53798&token=03d85092-1323-4cca-a85e-605a0201e689&version=O16GA&source=AMC&StoreId=CFQ7TTC0K5DM
-
-# Mouse with borders
-# wget https://www.microsoft.com/en-ca/download/confirmation.aspx?id=35460
+# Install each package
+foreach ($package in $packages) {
+    Write-Output "Installing $package..."
+    choco install $package -y
+}
