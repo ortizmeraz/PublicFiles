d6a2ab5d6d0cb5212008d5400f32523712bf9d74
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-05-18 11:49:38 -0400

    Update Download.sh

---
+++ Docker/Download.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/ortizmeraz/PublicFiles/refs/heads/main/Docker/Services-compose.yml
+wget https://raw.githubusercontent.com/ortizmeraz/PublicFiles/refs/heads/main/Docker/VPN_compose.yml
+wget https://raw.githubusercontent.com/ortizmeraz/PublicFiles/refs/heads/main/Docker/portainer-compose.yml
