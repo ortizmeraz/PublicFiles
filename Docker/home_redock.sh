878745d48b5ce3a8115959eee43029da9c97a938
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-05-29 20:14:19 -0400

    Update home_redock.sh

--- Docker/home_redock.sh
+++ Docker/home_redock.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-# ensure we’re in ~/docker
-cd "$(dirname "$0")"
 
 # stop & remove all containers
-/usr/bin/docker stop "$(/usr/bin/docker ps -aq)" || true
-/usr/bin/docker rm   "$(/usr/bin/docker ps -aq)" || true
+docker stop "$(docker ps -aq)"
+sleep 1
+docker rm   "$(docker ps -aq)"
+sleep 1
 
 # bring up your stacks
-/usr/local/bin/docker-compose -f ./docker/portainer-compose.yml up -d --remove-orphans
-/usr/local/bin/docker-compose -f ./docker/VPN_compose.yml      up -d --remove-orphans
-/usr/local/bin/docker-compose -f ./docker/Services-compose.yml up -d --remove-orphans
+docker-compose -f portainer-compose.yml up -d --remove-orphans
+sleep 1
+docker-compose -f VPN_compose.yml      up -d --remove-orphans
+sleep 1
+docker-compose -f Services-compose.yml up -d --remove-orphans
+sleep 1
