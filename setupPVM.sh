09657dd2761431d83aca88e31d4b44d7f733326e
Omar Ortiz Meraz <omar@ortizmeraz.com>
2025-06-26 11:42:30 -0400

    Update setupPVM.sh

--- setupPVM.sh
+++ setupPVM.sh
@@ -3,12 +3,13 @@ sudo apt upgrade -y
 sudo apt install nala -y
 sudo nala install qemu-guest-agent -y
 
+
 sudo nala install neofetch -y
 sudo nala install python3 -y
 sudo nala install curl -y
 sudo nala install ranger -y
 sudo nala install btop -y
-sudo nala install exa -y
+sudo nala install eza -y
 sudo nala install tmux -y
 sudo nala install fish -y
 sudo nala install bat -y
@@ -16,12 +17,11 @@ sudo nala install python3-pip -y
 
 curl -sS https://starship.rs/install.sh | sh
 
-mkdir -p ~/.config 
-mkdir -p ~/.config/tmux
-mkdir -p ~/Shared
 
-sudo chmod 777 ~/Shared -R
- 
+mkdir -p ~/.config 
+
+
+
 rm ~/.bashrc
 curl -o ~/.bashrc https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/.bashrc
 
@@ -31,31 +31,19 @@ curl -o ~/.config/fish/config.fish https://raw.githubusercontent.com/ortizmeraz/
 rm ~/.config/starship.toml
 curl -o ~/.config/starship.toml https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/starship.toml
 
-# rm /etc/samba/smb.conf
-# curl -o /etc/samba/smb.conf https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/smb.conf
-
-
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+mkdir -p ~/.config/tmux
+
 rm ~/.tmux.conf.local
 curl -o ~/.tmux.conf.local https://raw.githubusercontent.com/ortizmeraz/PublicFiles/main/tmux.conf.local
 
-sudo systemclt enable qemu-guest-agent
-sudo systemclt start qemu-guest-agent
 
 sudo apt install fonts-firacode
 fc-cache -f -v
 
-
-echo "[Shared]
-   path = ~/Shared
-   writable = yes
-   guest ok = yes
-   read only = no" | sudo tee -a /etc/samba/smb.conf
-
-
-
+wget https://raw.githubusercontent.com/ortizmeraz/PublicFiles/refs/heads/main/.bashrc
 sudo reboot
